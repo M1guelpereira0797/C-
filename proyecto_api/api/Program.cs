4e4a2dac5ea10c44faac06fc40dd904e99fe546a
M1guelpereira0797 <110005364+M1guelpereira0797@users.noreply.github.com>
2024-04-29 14:11:19 -0400

    probando
    haciendo ajustentes con el console.writeline y con el console.readline

--- proyecto_api/api/Program.cs
+++ proyecto_api/api/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+string mensaje = "hoy es 29/04/2024";
+int numero = 4324234;
+Console.WriteLine(mensaje + numero);
+
+Console.WriteLine("Escribe tu nombre: ");
+string nombre = Console.ReadLine();
+Console.WriteLine(nombre);
