3e00487d7ec99a1403580afe9a19dbb8056449e1
M1guelpereira0797 <110005364+M1guelpereira0797@users.noreply.github.com>
2024-04-29 13:54:01 -0400

    proceso1
    colocando hola mundo y unos numeros de prueba

--- proyecto_coder_hpuse_nuevo/Program.cs
+++ proyecto_coder_hpuse_nuevo/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string mensaje= "HOLA MUNDO";
+int numero = 434234234;
+
+Console.WriteLine(mensaje  +  numero );
